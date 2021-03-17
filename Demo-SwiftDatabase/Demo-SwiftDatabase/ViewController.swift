1f8f272bdec288a154a3500f2ae441ccacd922bb
Ricardo Rauber <ricardo.rauber@poatek.com>
2021-03-17 08:49:48 -0300

    Added the Demo project

---
+++ Demo-SwiftDatabase/Demo-SwiftDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo-SwiftDatabase
+//
+//  Created by Poatek on 17/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
