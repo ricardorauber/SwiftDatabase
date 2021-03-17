181593c6a8d85eb51770c1a017ad31539566475a
Ricardo Rauber <ricardorauber@gmail.com>
2021-03-17 09:21:10 -0300

    Added the Insert method for a single row

--- Classes/SwiftDatabase.swift
+++ Classes/SwiftDatabase.swift
@@ -2,7 +2,23 @@ import Foundation
 
 public class SwiftDatabase {
     
-    func dummy() -> Bool {
+    // MARK: - Properties
+
+    var data: [String: [Any]] = [:]
+}
+
+// MARK: - CRUD
+extension SwiftDatabase {
+
+    @discardableResult
+    func insert<Item: Codable & Equatable>(on name: String? = nil,
+                                           item: Item) -> Bool {
+        
+        let name = name ?? String(describing: type(of: item))
+        if data[name] == nil {
+            data[name] = []
+        }
+        data[name]?.append(item)
         return true
     }
 }
--- Tests/SwiftDatabaseTests.swift
+++ Tests/SwiftDatabaseTests.swift
@@ -10,16 +10,49 @@ class SwiftDatabaseTests: QuickSpec {
         
         describe("SwiftDatabase") {
             
-            context("dummy") {
+            context("CRUD") {
                     
                 beforeEach {
                     database = SwiftDatabase()
                 }
                 
-                it("should do something") {
-                    expect(database.dummy()).to(beTrue())
+                context("Insert") {
+                
+                    it("should insert a row without the table name") {
+                        let row = Person(id: 1, name: "Ricardo", age: 35)
+                        let result = database.insert(item: row)
+                        expect(result).to(beTrue())
+                        expect(database.data["Person"]?.count) == 1
+                    }
+                    
+                    it("should insert a row with the table name") {
+                        let row = Person(id: 1, name: "Ricardo", age: 35)
+                        let result = database.insert(on: "people", item: row)
+                        expect(result).to(beTrue())
+                        expect(database.data["people"]?.count) == 1
+                    }
+                    
+                    it("should insert a new row when the table already exists") {
+                        var result = database.insert(item: Person(id: 1, name: "Ricardo", age: 35))
+                        expect(result).to(beTrue())
+                        expect(database.data["Person"]?.count) == 1
+                        result = database.insert(item: Person(id: 2, name: "Paul", age: 40))
+                        expect(result).to(beTrue())
+                        expect(database.data["Person"]?.count) == 2
+                    }
                 }
             }
         }
+    }
+}
+
+// MARK: - Test Helpers
+private struct Person: Codable, Equatable {
+    var id: Int
+    var name: String
+    var age: Int
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.id == rhs.id
     }
 }
